2e16967bf33f6bf299e5447f0adb9351d77ffebb
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-09-29 18:23:58 +0700

    12. init graphql

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expresGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expresGraphQL({
+  graphiql: true
+}));
 
 app.listen(4000, ()=>{
   console.log('Listening on Port 4000');
