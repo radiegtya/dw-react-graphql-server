760e8b7733737df7f7ffe0201ec1465634387733
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-10-04 16:54:59 +0700

    adding cors

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const schema = require('./schema/index');
 
 const app = express();
 
+app.use("/graphql", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use('/graphql', expresGraphQL({
   schema,
   graphiql: true
